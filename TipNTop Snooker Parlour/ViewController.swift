fac9fb50e897f0a9acc3eb534ce78fdec97ea47d
Dineshkumar <gdine89@Dineshkumars-MacBook-Air.local>
2015-05-11 18:45:55 +0530

    Initial Commit

---
+++ TipNTop Snooker Parlour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipNTop Snooker Parlour
+//
+//  Created by Dineshkumar on 11/05/15.
+//  Copyright (c) 2015 TipNTop Cue Sports. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TipNTop Snooker ParlourTests/TipNTop_Snooker_ParlourTests.swift
@@ -0,0 +1,36 @@
+//
+//  TipNTop_Snooker_ParlourTests.swift
+//  TipNTop Snooker ParlourTests
+//
+//  Created by Dineshkumar on 11/05/15.
+//  Copyright (c) 2015 TipNTop Cue Sports. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TipNTop_Snooker_ParlourTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
